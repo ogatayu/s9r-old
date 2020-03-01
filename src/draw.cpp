81f7958070770cc33b49fe2e53a74e24facf741a
Yuya Ogata <y3o4xx@gmail.com>
2020-03-01 12:45:19 +0900

    drawing waveform sample

--- src/draw.cpp
+++ src/draw.cpp
@@ -3,12 +3,18 @@
  */
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h> // for rand
 
 #include "draw.h"
 
 #include "nanovg.h"
 #define NANOVG_GLES3_IMPLEMENTATION
 #include "nanovg_gl.h"
+
+
+#define PI (3.141592653589793238462643383279f)
+#define SAMPLE_NUM (256)
 
 /**
  * @brief Initialize
@@ -23,6 +29,8 @@ bool Draw::Initialize()
  */
 void Draw::Start()
 {
+    static float data[SAMPLE_NUM] = { 0 };
+
     if (!glfwInit()) {
         return;
     }
@@ -40,25 +48,38 @@ void Draw::Start()
         glfwTerminate();
         return;
     }
-    int width = 512;
-    int height = 512;
-    float pixelRatio = 1.0;
 
     glfwSwapInterval(1);
 
+    float width = 320, height = 240;
+    float phase = 0;
     while (!glfwWindowShouldClose(glfw_window_)) {
-        glClearColor(0.8f, 0.8f, 0.8f, 0.0f);
+        for(int ix=0; ix<SAMPLE_NUM; ix++) {
+            data[ix]  = sin((phase++/50) * PI);
+            //data[ix] *= (float)(rand()-(RAND_MAX/2))/(float)RAND_MAX;
+        }
+
+        glClearColor(0.1f, 0.1f, 0.2f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        nvgBeginFrame(vg, width, height, pixelRatio);
+        nvgBeginFrame(vg, width, height, 1.0);
+        {
+            nvgBeginPath(vg);
+            {
+                nvgMoveTo(vg, 0, data[0] + 100);
 
+                float w = width / SAMPLE_NUM;
+                for(int ix=0; ix<SAMPLE_NUM; ix++) {
+                    nvgLineTo( vg, (float)ix * w, (data[ix]*100.0f) + 120 );
+                }
 
-        nvgBeginPath(vg);
-        nvgRect(vg, 10,10, 120,30);
-        nvgFillColor(vg, nvgRGBA(255,192,0,255));
-        nvgFill(vg);
-
-
+                nvgStrokeColor(vg, nvgRGBA(255,255,255,200));
+                nvgStrokeWidth(vg, 2.0f);
+                nvgLineJoin(vg, NVG_ROUND);
+                nvgLineCap(vg, NVG_ROUND);
+                nvgStroke(vg);
+            }
+        }
         nvgEndFrame(vg);
 
         glfwSwapBuffers(glfw_window_);
