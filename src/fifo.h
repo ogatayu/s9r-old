5d572a388e93966b320aeed760abb5bf3824675d
Yuya Ogata <y3o4xx@gmail.com>
2020-03-01 22:29:51 +0900

    add fifo test

--- src/fifo.h
+++ src/fifo.h
@@ -5,10 +5,6 @@
 
 class FIFO {
 private:
-public:
-    FIFO( int capacity, int block_size );
-    ~FIFO( void );
-
     void         *buffer_;    /*!< FIFOのデータ格納先 */
     int           blockSize_; /*!< buffer_ の読み書き単位サイズ[byte] */
     int           capacity_;  /*!< FIFOに格納できるデータの数 */
@@ -16,7 +12,11 @@ public:
     unsigned long tail_;      /*!< 次にデータを格納する場所へのオフセット */
     unsigned long head_;
 
+public:
+    FIFO( int capacity, int block_size );
+    ~FIFO( void );
+
     void Put( void *data );
-    int  Get( void *data );
+    void Get( void *data );
     void SnoopFromTail( void *data, int num );
 };
