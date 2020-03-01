2a1ddcf3c621f2ef72ed2e4613da7544c21ddbc0
Yuya Ogata <y3o4xx@gmail.com>
2020-03-01 15:20:42 +0900

    fix 正しい波形を描画

--- src/fifo.h
+++ src/fifo.h
@@ -6,16 +6,17 @@
 class FIFO {
 private:
 public:
-    FIFO( size_t capacity, size_t block_size );
+    FIFO( int capacity, int block_size );
     ~FIFO( void );
 
     void         *buffer_;    /*!< FIFOのデータ格納先 */
-    size_t        blockSize_; /*!< buffer_ の読み書き単位サイズ[byte] */
-    size_t        capacity_;  /*!< FIFOに格納できるデータの数 */
-    size_t        length_;    /*!< 現在FIFOに格納されているデータの数 */
+    int           blockSize_; /*!< buffer_ の読み書き単位サイズ[byte] */
+    int           capacity_;  /*!< FIFOに格納できるデータの数 */
+    int           length_;    /*!< 現在FIFOに格納されているデータの数 */
     unsigned long tail_;      /*!< 次にデータを格納する場所へのオフセット */
     unsigned long head_;
 
     void Put( void *data );
     int  Get( void *data );
+    void SnoopFromTail( void *data, int num );
 };
